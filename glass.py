04bb9b5665e4c36d9fa7282351deaf9321e25d22
Kramer <kdanielharrison@gmail.com>
2025-03-03 19:10:21 +0100

    Add Glass module defining valid glasses for lens system

---
+++ glass.py
@@ -0,0 +1,31 @@
+"""LensRL - Glass module.
+
+This module defines the valid glasses that can be used in the lens system.
+Later, additional glass or material-related functionality may be added.
+
+Kramer Harrison, 2025
+"""
+# Schott glass only
+VALID_GLASSES = [
+    'N-BALF5', 'N-PK52A', 'N-SSK2', 'N-LAF3', 'N-SK15',
+    'N-ZK7', 'N-LAF34', 'N-LAF2', 'N-FK5', 'N-SF6',
+    'N-BALF4', 'N-SSK5', 'N-BAK4', 'N-BAK2', 'N-SF14',
+    'N-LAK9', 'N-FK58', 'N-SK2HT', 'N-SF6HT', 'N-BAF10',
+    'N-PSK3', 'N-F2', 'N-KZFS11', 'N-LASF45', 'N-LASF40',
+    'N-SF56', 'N-PK51', 'N-LASF46B', 'N-K5', 'N-LAK21',
+    'N-SF57', 'N-KF9', 'N-SK2', 'N-LAK33B', 'N-SF8',
+    'N-LASF31A', 'N-BAF4', 'N-KZFS4HT', 'N-SF1', 'N-LAF7',
+    'N-BAK1', 'N-LAK22', 'N-SF15', 'N-BAF51', 'N-LAF33',
+    'N-SF66', 'N-SK11', 'N-SF6HTultra', 'N-SF64', 'N-SF57HTultra',
+    'N-LASF43', 'N-SF11', 'N-BK7HTi', 'N-BASF64', 'N-FK51A',
+    'N-LAF35', 'N-SF10', 'N-BK7HT', 'N-SK16', 'N-LAF21',
+    'N-SF4', 'N-LASF45HT', 'N-KZFS2', 'N-LAK8', 'N-LAF36',
+    'N-SK14', 'N-KZFS5', 'N-LAK10', 'N-SK4', 'N-BK7',
+    'N-BASF2', 'N-ZK7A', 'N-SK5', 'N-SF19', 'N-PSK53A',
+    'N-LASF44', 'N-PSK53', 'N-LASF41', 'N-LASF9', 'N-SK10',
+    'N-SSK8', 'N-BAK4HT', 'N-FK51', 'N-LAK7', 'N-LAK34',
+    'N-KZFS8', 'N-LAF32', 'N-SF2', 'N-LASF46A', 'N-SF5',
+    'N-LASF46', 'N-LAK12', 'N-KZFS4', 'N-BAF52', 'N-BAF3',
+    'N-SF57HT', 'N-LASF9HT', 'N-LASF31', 'N-LAK33A', 'N-LAK14',
+    'N-BK10'
+]
